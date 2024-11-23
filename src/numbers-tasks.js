f93889c9194baca86c782791ed943571202ab474
Konstantin Kienya <kienyamail@gmail.com>
2024-11-24 00:05:45 +0300

    feat: add getDistanceBetweenPoints task solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -68,8 +68,10 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const x = x2 - x1;
+  const y = y2 - y1;
+  return Math.sqrt(x * x + y * y);
 }
 
 /**
